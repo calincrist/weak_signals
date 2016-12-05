116606a1dcdb93584c5644f89731a4006f356ce7
Calin Cristian <calin.crist@gmail.com>
2016-12-06 01:57:20 +0200

    Re-structured django project.

--- spln/spln/wsgi.py
+++ spln/spln/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for spnlproject project.
+WSGI config for spln project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spnlproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spln.settings")
 
 application = get_wsgi_application()
