d67bf595c5f9557249fcab5fd4596ed31ecbce52
Calin Cristian <calin.crist@gmail.com>
2017-01-11 00:18:54 +0200

    Fixed static files.

--- spln/spln/urls.py
+++ spln/spln/urls.py
@@ -24,7 +24,7 @@ from splnapp import urls as app_urls
 urlpatterns = [
     url(r'^spln/', include(app_urls)),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
